2878c9fc1a6e67315b65565dead2c2a137a9142b
al fang <Al@Alberts-MBP.wireless.yale.internal>
2017-03-30 19:19:40 -0400

    update site

--- assets/js/showhide.js
+++
@@ -1,3 +0,0 @@
-function openClose(theID) {
-if (document.getElementById(theID).style.display == "block") { document.getElementById(theID).style.display = "none" }
-else { document.getElementById(theID).style.display = "block" } }
