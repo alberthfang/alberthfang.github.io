221c552b2b0643342f62a6d92200328721d9c9af
al fang <Al@Alberts-MBP.wireless.yale.internal>
2017-03-30 19:20:00 -0400

    add showhide.js

---
+++ assets/js/showhide.js
@@ -0,0 +1,3 @@
+function openClose(theID) {
+if (document.getElementById(theID).style.display == "block") { document.getElementById(theID).style.display = "none" }
+else { document.getElementById(theID).style.display = "block" } }
